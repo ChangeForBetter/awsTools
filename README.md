bf53db08d1c473e0f2fa2c860e3ece526fdfd495
Liu Hao <wustliuhao@gmail.com>
2019-05-30 17:43:59 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+[TOC]
+
+## Requirements
+
+```bash
+brew install jq
+pip install icdiff
+```
+
+## Usage
+
+```bash
+source awsTools.sh
+```
+
+## Reference
+
+* [aws cli reference](https://docs.aws.amazon.com/cli/latest/reference/)
+* [jq manual](https://stedolan.github.io/jq/manual/)
+
---
+++ awsTools.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function ecstaskdesc() {
+    aws ecs describe-task-definition --task-definition $1
+}
+
+function ecstaskregister() {
+    dirName=$(cd $(dirname $1); pwd)
+    fileName=$(basename $1)
+    parameter=file://$dirName/$fileName
+    # echo $parameter
+    aws ecs register-task-definition --cli-input-json $parameter
+}
+
+function ecstaskdiff() {
+    tmpDir=/tmp/awsTools
+    if [ ! -d ${tmpDir} ]; then
+        mkdir ${tmpDir}
+    fi
+
+    taskDefinition_a=$1
+    ecstaskdesc $taskDefinition_a 2>&1 > $tmpDir/$taskDefinition_a
+
+    if [ -z "$2" ]; then
+        taskFamily=$(jq '.taskDefinition.family' $tmpDir/$taskDefinition_a | sed s'/"//g')
+        latestRevison=$(jq '.taskDefinition.revision' $tmpDir/$taskDefinition_a)
+        taskDefinition_b=$taskFamily:$(( $latestRevison - 1 ))
+    else
+        taskDefinition_b=$2
+    fi
+    ecstaskdesc $taskDefinition_b 2>&1 > $tmpDir/$taskDefinition_b
+
+    icdiff $tmpDir/$taskDefinition_a $tmpDir/$taskDefinition_b
+}
+
